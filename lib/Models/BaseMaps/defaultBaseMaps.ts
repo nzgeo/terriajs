337a741bb977e621de758d0451332a4c3c19d06a
Dan Scanlen <d.scanlen@gmail.com>
2022-06-09 10:36:34 +1200

    fix: readd basemaps to terriajs

--- lib/Models/BaseMaps/defaultBaseMaps.ts
+++ lib/Models/BaseMaps/defaultBaseMaps.ts
@@ -18,81 +18,81 @@ export function defaultBaseMaps(terria: Terria): BaseMapJson[] {
     image: "build/TerriaJS/images/basemap-osm-bright.png",
     contrastColor: "#000000"
   });
-  // baseMaps.push({
-  //   item: {
-  //     id: "osm_basic",
-  //     name: "OSM Basic",
-  //     type: "wmts",
-  //     layer: "osm_basic",
-  //     url:
-  //       "http://localhost/mapproxy/service?REQUEST=GetCapabilities&SERVICE=WMTS",
-  //       // "/mapproxy/service?REQUEST=GetCapabilities&SERVICE=WMTS",
-  //     maximumLevel: 22,
-  //     opacity: 1.0
-  //   },
-  //   image: "build/TerriaJS/images/basemap-osm-basic.png",
-  //   contrastColor: "#000000"
-  // });
-  // baseMaps.push({
-  //   item: {
-  //     id: "osm_positron",
-  //     name: "OSM Position",
-  //     type: "wmts",
-  //     layer: "osm_positron",
-  //     url:
-  //       "http://localhost/mapproxy/service?REQUEST=GetCapabilities&SERVICE=WMTS",
-  //       // "/mapproxy/service?REQUEST=GetCapabilities&SERVICE=WMTS",
-  //     maximumLevel: 22,
-  //     opacity: 1.0
-  //   },
-  //   image: "build/TerriaJS/images/basemap-osm-positron.png",
-  //   contrastColor: "#000000"
-  // });
-  // baseMaps.push({
-  //   item: {
-  //     id: "osm_street",
-  //     name: "OSM Street",
-  //     type: "wmts",
-  //     layer: "osm_street",
-  //     url:
-  //       "http://localhost/mapproxy/service?REQUEST=GetCapabilities&SERVICE=WMTS",
-  //       // "/mapproxy/service?REQUEST=GetCapabilities&SERVICE=WMTS",
-  //     maximumLevel: 22,
-  //     opacity: 1.0
-  //   },
-  //   image: "build/TerriaJS/images/basemap-osm-street.png",
-  //   contrastColor: "#000000"
-  // });
-  // baseMaps.push({
-  //   item: {
-  //     id: "osm_terrain",
-  //     name: "OSM Terrain",
-  //     type: "wmts",
-  //     layer: "osm_terrain",
-  //     url:
-  //       "http://localhost/mapproxy/service?REQUEST=GetCapabilities&SERVICE=WMTS",
-  //       // "/mapproxy/service?REQUEST=GetCapabilities&SERVICE=WMTS",
-  //     maximumLevel: 22,
-  //     opacity: 1.0
-  //   },
-  //   image: "build/TerriaJS/images/basemap-osm-terrain.png",
-  //   contrastColor: "#000000"
-  // });
-  // baseMaps.push({
-  //   item: {
-  //     id: "imagery_label",
-  //     name: "Imagery",
-  //     type: "wmts",
-  //     layer: "imagery_label",
-  //     url:
-  //       "http://localhost/mapproxy/service?REQUEST=GetCapabilities&SERVICE=WMTS",
-  //       // "/mapproxy/service?REQUEST=GetCapabilities&SERVICE=WMTS",
-  //     maximumLevel: 22,
-  //     opacity: 1.0
-  //   },
-  //   image: "build/TerriaJS/images/basemap-imagery.png",
-  //   contrastColor: "#000000"
-  // });
+  baseMaps.push({
+    item: {
+      id: "osm_basic",
+      name: "OSM Basic",
+      type: "wmts",
+      layer: "osm_basic",
+      url:
+        "http://localhost/mapproxy/service?REQUEST=GetCapabilities&SERVICE=WMTS",
+        // "/mapproxy/service?REQUEST=GetCapabilities&SERVICE=WMTS",
+      maximumLevel: 22,
+      opacity: 1.0
+    },
+    image: "build/TerriaJS/images/basemap-osm-basic.png",
+    contrastColor: "#000000"
+  });
+  baseMaps.push({
+    item: {
+      id: "osm_positron",
+      name: "OSM Position",
+      type: "wmts",
+      layer: "osm_positron",
+      url:
+        "http://localhost/mapproxy/service?REQUEST=GetCapabilities&SERVICE=WMTS",
+        // "/mapproxy/service?REQUEST=GetCapabilities&SERVICE=WMTS",
+      maximumLevel: 22,
+      opacity: 1.0
+    },
+    image: "build/TerriaJS/images/basemap-osm-positron.png",
+    contrastColor: "#000000"
+  });
+  baseMaps.push({
+    item: {
+      id: "osm_street",
+      name: "OSM Street",
+      type: "wmts",
+      layer: "osm_street",
+      url:
+        "http://localhost/mapproxy/service?REQUEST=GetCapabilities&SERVICE=WMTS",
+        // "/mapproxy/service?REQUEST=GetCapabilities&SERVICE=WMTS",
+      maximumLevel: 22,
+      opacity: 1.0
+    },
+    image: "build/TerriaJS/images/basemap-osm-street.png",
+    contrastColor: "#000000"
+  });
+  baseMaps.push({
+    item: {
+      id: "osm_terrain",
+      name: "OSM Terrain",
+      type: "wmts",
+      layer: "osm_terrain",
+      url:
+        "http://localhost/mapproxy/service?REQUEST=GetCapabilities&SERVICE=WMTS",
+        // "/mapproxy/service?REQUEST=GetCapabilities&SERVICE=WMTS",
+      maximumLevel: 22,
+      opacity: 1.0
+    },
+    image: "build/TerriaJS/images/basemap-osm-terrain.png",
+    contrastColor: "#000000"
+  });
+  baseMaps.push({
+    item: {
+      id: "imagery_label",
+      name: "Imagery",
+      type: "wmts",
+      layer: "imagery_label",
+      url:
+        "http://localhost/mapproxy/service?REQUEST=GetCapabilities&SERVICE=WMTS",
+        // "/mapproxy/service?REQUEST=GetCapabilities&SERVICE=WMTS",
+      maximumLevel: 22,
+      opacity: 1.0
+    },
+    image: "build/TerriaJS/images/basemap-imagery.png",
+    contrastColor: "#000000"
+  });
 
   return baseMaps;
 }
