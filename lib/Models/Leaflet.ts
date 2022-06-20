66512efec6fba657d72eaa127b4b3f5a0eae7995
Dan Scanlen <d.scanlen@gmail.com>
2022-06-20 13:47:14 +1200

    revert: undo logging mouseEvent to console

--- lib/Models/Leaflet.ts
+++ lib/Models/Leaflet.ts
@@ -250,7 +250,6 @@ export default class Leaflet extends GlobeOrMap {
       // Update mouse coords on mouse move
       this.map.on("mousemove", (e: L.LeafletEvent) => {
         const mouseEvent = <L.LeafletMouseEvent>e;
-        console.log(mouseEvent)
         this.mouseCoords.updateCoordinatesFromLeaflet(
           this.terria,
           mouseEvent.originalEvent
