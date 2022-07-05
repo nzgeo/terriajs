4b2e97062ccd0e070f81adf29cc0ccdb65f73ee7
Dan Scanlen <d.scanlen@gmail.com>
2022-07-06 10:48:06 +1200

    test: XHR loading mechanism only

--- lib/Models/SearchProviders/CGSSearchProvider.ts
+++ lib/Models/SearchProviders/CGSSearchProvider.ts
@@ -9,6 +9,9 @@ import SearchResult from "./SearchResult";
 import Terria from "../Terria";
 import SearchProviderResults from "./SearchProviderResults";
 import loadWithXhr from "../../Core/loadWithXhr"
+import zoomRectangleFromPoint from "../../Map/Vector/zoomRectangleFromPoint"
+
+let APIKEY =  "Api-Key j5SJa2YsiwOjzRPmVCywV"
 
  interface CGSSearchProviderOptions {
     terria: Terria;
@@ -23,7 +26,6 @@ export default class CGSSearchProvider extends SearchProvider{
     readonly terria: Terria;
     @observable url: string;
     @observable auth: string | undefined;
-    @observable searchTerm: string | undefined;
     @observable maxResults: number;
     @observable flightDurationSeconds: number;
 
@@ -33,8 +35,7 @@ export default class CGSSearchProvider extends SearchProvider{
         this.terria = options.terria;
         this.name = "Locations"
         this.url = defaultValue(options.url, "/search/");
-        this.auth = "Api-Key j5SJa2YsiwOjzRPmVCywV"
-        this.searchTerm = options.searchTerm;
+        this.auth = APIKEY
         this.maxResults = 5
         this.flightDurationSeconds = defaultValue(options.flightDurationSeconds, 1.5);
 
@@ -42,7 +43,8 @@ export default class CGSSearchProvider extends SearchProvider{
             console.warn("The " + this.name + " geocoder will always return no results because the CGS Search API Key has not been configured.");
         }
     }
-    protected doSearch(searchText: string, searchResults: SearchProviderResults): Promise<void> {
+
+    protected doSearch(searchText: string, searchResults: SearchProviderResults): Promise<any> {
         searchResults.results.length = 0;
         searchResults.message = undefined;
 
@@ -50,73 +52,53 @@ export default class CGSSearchProvider extends SearchProvider{
             return Promise.resolve();
         }
 
-        const promise: Promise<any> = loadJsonp(
-            new Resource({
-                url: this.url + "api/v1/locations/search", 
-                queryParameters: {query: searchText, limit: this.maxResults, key: this.auth}
-            })
-          );
+        const promise: Promise<any> = loadWithXhr({
+            url: "/search/api/v1/locations/search?query=" + searchText + "&limit=" + this.maxResults,
+            method: "GET",
+            headers: { "Authorization": this.auth },
+            responseType: "json"
+        });
 
         return promise
-            .then (result => {
+            .then(data => {
                 if (searchResults.isCanceled) {
                     // A new search has superseded this one, so ignore the result.
                     return;
                 }
-                if (result.resourceSets.length === 0) {
-                    searchResults.message = ("WARNING: result resourceSet length === 0");
+        
+                if (data.results.length === 0) {
+                    searchResults.message = "viewModels.searchNoLocations";
                     return;
                 }
-
-                let resourceSet = result.resourceSets[0];
-                if (resourceSet.resources.length === 0) {
-                    searchResults.message = ("WARNING: result resourceSet resources length === 0");
-                    return;
-                }
-
-                const list: any[] = [];
-
-                for (let i = 0; i < resourceSet.resources.length; ++i) {
-                    const resource = resourceSet.resources[i];
+                
+                let results = [];
+                for(let i = 0; i <data.results.length; i++) {
+                    let resource = data.results[i]
                     let name = resource.name;
-                    if (!defined(name)) {
-                      continue;
-                    }
-                    list.push(
-                        new SearchResult({
-                          name: name,
-                          clickAction: createZoomToFunction(this, resource),
-                          location: {
-                            latitude: resource.point.coordinates[0],
-                            longitude: resource.point.coordinates[1]
-                          }
-                        })
-                    );
+                    let xhttp = new XMLHttpRequest();
+                    xhttp.onreadystatechange = function() {
+                        if (this.readyState == 4 && this.status == 200) {
+                            let response = JSON.parse(xhttp.responseText)
+                            let result = {
+                                name: name,
+                                isImportant: true,
+                                location: {
+                                    longitude: response.geojson.bbox[2] - Math.abs(response.geojson.bbox[2] - response.geojson.bbox[0]) / 2,
+                                    latitude: response.geojson.bbox[3] - Math.abs(response.geojson.bbox[3] - response.geojson.bbox[1]) / 2
+                                }
+                            };
+                            results.push(result);
+                        }
+                    };
+                    xhttp.open("GET", "/search/api/v1/locations/geometry?query=" + name, false);
+                    xhttp.setRequestHeader("Authorization", APIKEY);
+                    xhttp.send();
                 }
-                runInAction(() => {
-                    searchResults.results.push(...list);
-                });
-
-                if (searchResults.results.length === 0) {
-                    searchResults.message = "WARNING: searchResults === 0";
-                }
-            })
-            .catch(() => {
-                if (searchResults.isCanceled) {
-                    // A new search has superseded this one, so ignore the result.
-                    return;
-                }
-                searchResults.message = ("WARNING: searchErrorOccurred");
             });
+        }
     }
-}
+    
 
-function createZoomToFunction(model: CGSSearchProvider, resource: any) {
-    const [south, west, north, east] = resource.bbox;
-    const rectangle = Rectangle.fromDegrees(west, south, east, north);
 
-    return function() {
-        const terria = model.terria;
-        terria.currentViewer.zoomTo(rectangle, model.flightDurationSeconds);
-    };
-}
+
+
